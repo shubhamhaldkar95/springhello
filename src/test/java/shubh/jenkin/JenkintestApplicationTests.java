37c06761e0c05b74718e378c2cc191f8046cce96
shubhamhaldkar95 <shubhamhaldkar95@gmail.com>
2020-03-14 13:53:17 +0530

    init project java

---
+++ src/test/java/shubh/jenkin/JenkintestApplicationTests.java
@@ -0,0 +1,13 @@
+package shubh.jenkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkintestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
